7193544678384b2fc9fd43bef254a4a1552597df
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-07-18 08:13:26 -0700

    Add droplet stop message variants

--- lib/cf/interface/droplet_stop_message.rb
+++ lib/cf/interface/droplet_stop_message.rb
@@ -1,6 +1,6 @@
 module CF::Interface
   DropletStopMessage = Struct.new(:app_guid) do
-    # taken from VCAP::CloudController::DeaClient#dea_publish_stop
+    # taken from VCAP::CloudController::DeaClient#stop
     def serialize
       {
         droplet: app_guid
@@ -10,6 +10,41 @@ module CF::Interface
     def self.deserialize(serialized)
       parsed = JSON.parse(serialized)
       new(parsed.fetch("droplet"))
+    end
+  end
+
+  DropletStopInstancesMessage = Struct.new(:app_guid, :instance_guids) do
+    # taken from VCAP::CloudController::DeaClient#stop_instances
+    def serialize
+      {
+        droplet: app_guid,
+        instances: instance_guids
+      }.to_json
+    end
+
+    def self.deserialize(serialized)
+      parsed = JSON.parse(serialized)
+      new(parsed.fetch("droplet"), parsed.fetch("instances"))
+    end
+  end
+
+  DropletStopIndicesMessage = Struct.new(:app_guid, :app_version, :instance_indices) do
+    # taken from VCAP::CloudController::DeaClient#stop_indices
+    def serialize
+      {
+        droplet: app_guid,
+        version: app_version,
+        indices: instance_indices
+      }.to_json
+    end
+
+    def self.deserialize(serialized)
+      parsed = JSON.parse(serialized)
+      new(
+        parsed.fetch("droplet"),
+        parsed.fetch("version"),
+        parsed.fetch("indices")
+      )
     end
   end
 end
--- spec/droplet_stop_message_spec.rb
+++ spec/droplet_stop_message_spec.rb
@@ -12,3 +12,30 @@ describe CF::Interface::DropletStopMessage do
     end
   end
 end
+
+describe CF::Interface::DropletStopInstancesMessage do
+  describe "serialization" do
+    it "can round trip properly" do
+      original = described_class.new("an app guid", %w(instance-guid1 instance-guid2))
+      clone = described_class.deserialize(original.serialize)
+
+      expect(original).not_to equal(clone)
+      expect(clone.app_guid).to eq("an app guid")
+      expect(clone.instance_guids).to eq(%w(instance-guid1 instance-guid2))
+    end
+  end
+end
+
+describe CF::Interface::DropletStopIndicesMessage do
+  describe "serialization" do
+    it "can round trip properly" do
+      original = described_class.new("an app guid", "app version", [3, 4])
+      clone = described_class.deserialize(original.serialize)
+
+      expect(original).not_to equal(clone)
+      expect(clone.app_guid).to eq("an app guid")
+      expect(clone.app_version).to eq("app version")
+      expect(clone.instance_indices).to eq([3, 4])
+    end
+  end
+end
